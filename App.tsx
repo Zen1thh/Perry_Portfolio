27e1e56f110818a8e3a3352f417fc7ff242fb50c
Zen1th <bambiiakielah@gmail.com>
2025-11-20 22:02:30 +0800

    Remove ParticleBackground and restore system cursor for better performance

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ParticleBackground from './components/ParticleBackground';
 import Hero from './components/Hero';
 import About from './components/About';
 import TechStack from './components/TechStack';
@@ -8,13 +7,10 @@ import Contact from './components/Contact';
 import AIChat from './components/AIChat';
 import Navbar from './components/Navbar';
 import SmoothScroll from './components/SmoothScroll';
-import CustomCursor from './components/CustomCursor';
 
 function App() {
   return (
     <div className="relative min-h-screen bg-black text-white selection:bg-white selection:text-black font-inter">
-      <CustomCursor />
-      <ParticleBackground />
       
       <SmoothScroll>
         <Navbar />
